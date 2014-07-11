a34229f1d88dc48278cba75e3c1ecaf8261ae17d
Henk Reder <henk@anestuary.com>
2014-07-11 16:09:57 -0400

    used doseq to get the names of sites in the dwarf-xml

--- src/dwarf_statements/parser.clj
+++ src/dwarf_statements/parser.clj
@@ -1,3 +1,22 @@
 (ns dwarf-statements.core
     "parse data from xml file"
   (:use [clojure.data.xml]))
+
+
+(defn parse-xml
+  "parse xml file at path"
+  [file-path]
+  (let [input-xml (clojure.java.io/reader file-path)]
+  (parse input-xml)))
+
+(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")]
+  (doseq [x (xml-seq (parse xml-file))
+    :when (= :sites (:tag x))]
+      (doseq [y (:content x)
+        :when (= :site (:tag y))]
+        (println (first (:content y))))))
+
+
+(parse-xml "resources/data/region2-legends.xml")
+
+((parse-xml "resources/data/test.xml"))
