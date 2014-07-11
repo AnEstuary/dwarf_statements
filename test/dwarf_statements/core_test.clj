a328ba143e8a688e6836b1e444e50e15b9c8d015
Milt Reder <milt@anestuary.com>
2014-07-11 10:20:57 -0400

    initial commit

---
+++ test/dwarf_statements/core_test.clj
@@ -0,0 +1,7 @@
+(ns dwarf-statements.core-test
+  (:require [clojure.test :refer :all]
+            [dwarf-statements.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
