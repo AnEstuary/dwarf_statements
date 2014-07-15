cd3f0b3b823ebbe52d92c510b8575cc530ed03f1
Henk Reder <henk@anestuary.com>
2014-07-15 17:55:46 -0400

    added a function for lazy sequencing

--- src/dwarf_statements/parser.clj
+++ src/dwarf_statements/parser.clj
@@ -1,38 +1,74 @@
 (ns dwarf-statements.core
     "parse data from xml file"
-  (:use [clojure.data.xml]))
+  (:use [clojure.data.zip.xml
+           :refer [xml-> xml1-> attr attr= text]]
+        [clojure.zip :as c-zip :refer [xml-zip]]))
 
 (require '[clojure.walk :as w])
 
-
-;(defn parse-xml
-;  "parse xml file at path"
-;  [file-path]
-;  (let [input-xml (clojure.java.io/reader file-path)]
-;  (parse input-xml)))
-
-;(def xml-data (xml-seq (parse-xml "resources/data/region2-legends.xml")))
 
 
 ; This let parses the xml file, reads the name of all sites in our XMLfile and puts them in a vector
 ; The plan is to use the principles from this code to parse our data for xAPI
 
-(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")
-      a-list (transient [])]
-  (doseq [x (xml-seq (parse xml-file))
-    :when (= :sites (:tag x))]
-      (doseq [y (:content x)
-        :when (= :site (:tag y))]
+;(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")
+;      a-list (transient [])]
+;  (doseq [x (xml-seq (parse xml-file))
+;    :when (= :sites (:tag x))]
+;      (doseq [y (:content x)
+;        :when (= :site (:tag y))]
+;          (doseq [nimi   (:content y)
+;                  tag    (:content y)
+;                  coords (:content y)
+;            :when (= :name   (:tag nimi))
+;            :when (= :id     (:tag tag))
+;            :when (= :coords (:tag coords))]
+;            (conj! a-list {:name (first (:content nimi)), :id (first (:content tag)), :coords (first (:content coords))}))))
+;  (for[i (range (count a-list))]
+;    (a-list i)))
+
+
+
+(defn xml-dwarf-site-parser
+  "This function parses information about sites from the given
+  dwarven XML file path and stores it on a list"
+  [final-list file-path]
+  (let [xml-file (clojure.java.io/reader file-path)]
+    (doseq [x (xml-seq (parse xml-file :coalescing false))
+      :when (= :sites (:tag x))]
+        (doseq [y (:content x)
+          :when (= :site (:tag y))]
           (doseq [nimi   (:content y)
                   tag    (:content y)
                   coords (:content y)
-            :when (= :name   (:tag nimi))
-            :when (= :id     (:tag tag))
-            :when (= :coords (:tag coords))]
-            (conj! a-list {:name (first (:content nimi)), :id (first (:content tag)), :coords (first (:content coords))}))))
-  (for[i (range (count a-list))]
-    (a-list i)))
+             :when (= :name (:tag nimi))
+             :when (= :id    (:tag tag))
+             :when (= :coords (:tag coords))]
+            (conj! final-list {:name (first (:content nimi)), :id (first (:content tag)), :coords (first (:content coords))})
+            )))))
 
+
+
+(defn sites [e]
+  (xml1-> (xml-zip element)
+          :sites
+          text))
+
+(with-open [rdr(clojure.java.io/reader "resources/data/region2-legends.xml")]
+        (doall (->> rdr
+            parse
+            :content
+            (filter #(= :pages (:tag %)))
+            (map sites))))
+
+
+
+(def parsed-site-data (transient []))
+(xml-dwarf-site-parser parsed-site-data "resources/data/region2-legends.xml")
+
+
+(for[i (range (count parsed-site-data))]
+    (parsed-site-data i))
 
 
 (let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")]
