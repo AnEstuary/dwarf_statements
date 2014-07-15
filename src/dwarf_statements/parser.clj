5c73f287ea166195637daa3087b700e27c998dd0
Henk Reder <henk@anestuary.com>
2014-07-15 11:35:40 -0400

    figured out how to properly parse data for clojure vectors

--- src/dwarf_statements/parser.clj
+++ src/dwarf_statements/parser.clj
@@ -2,23 +2,34 @@
     "parse data from xml file"
   (:use [clojure.data.xml]))
 
+(require '[clojure.walk :as w])
 
-(defn parse-xml
-  "parse xml file at path"
-  [file-path]
-  (let [input-xml (clojure.java.io/reader file-path)]
-  (parse input-xml)))
 
-(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")]
+;(defn parse-xml
+;  "parse xml file at path"
+;  [file-path]
+;  (let [input-xml (clojure.java.io/reader file-path)]
+;  (parse input-xml)))
+
+;(def xml-data (xml-seq (parse-xml "resources/data/region2-legends.xml")))
+
+
+; This let parses the xml file, reads the name of all sites in our XMLfile and puts them in a vector
+; The plan is to use the principles from this code to parse our data for xAPI
+
+(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")
+      a-list (transient [])]
   (doseq [x (xml-seq (parse xml-file))
     :when (= :sites (:tag x))]
       (doseq [y (:content x)
         :when (= :site (:tag y))]
           (doseq [z (:content y)
-            :when(= :name (:tag z))]
-              (println (first (:content z)))))))
+            :when (= :name (:tag z))]
+            (conj! a-list (first (:content z))))))
+  (for[i (range (count a-list))]
+    (a-list i)))
 
 
-(parse-xml "resources/data/region2-legends.xml")
 
-((parse-xml "resources/data/test.xml"))
+(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")]
+  (type(xml-seq (parse xml-file))))
