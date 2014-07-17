8fde2c6377960494bf7e7632473f9af7e0bbf0f2
Henk Reder <henk@anestuary.com>
2014-07-17 13:15:26 -0400

    got lazy evaluation working, need to figure out how to navigate xml file @ deepest points.

--- src/dwarf_statements/parser.clj
+++ src/dwarf_statements/parser.clj
@@ -1,7 +1,8 @@
 (ns dwarf-statements.core
     "parse data from xml file"
-  (:use [clojure.data.zip.xml
-           :refer [xml-> xml1-> attr attr= text]]
+  (:use [clojure.data.zip.xml :as c-d-z-xml
+                :refer [xml-> xml1-> attr attr= text]]
+        [clojure.data.xml :as c-d-xml :refer [parse]]
         [clojure.zip :as c-zip :refer [xml-zip]]))
 
 (require '[clojure.walk :as w])
@@ -11,65 +12,49 @@
 ; This let parses the xml file, reads the name of all sites in our XMLfile and puts them in a vector
 ; The plan is to use the principles from this code to parse our data for xAPI
 
-;(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")
-;      a-list (transient [])]
-;  (doseq [x (xml-seq (parse xml-file))
-;    :when (= :sites (:tag x))]
-;      (doseq [y (:content x)
-;        :when (= :site (:tag y))]
+
+
+;(defn xml-dwarf-site-parser
+; "This function parses information about sites from the given
+; dwarven XML file path and stores it on a list"
+;  [final-list file-path]
+;  (let [xml-file (clojure.java.io/reader file-path)]
+;    (doseq [x (xml-seq (parse xml-file :coalescing false))
+;      :when (= :sites (:tag x))]
+;        (doseq [y (:content x)
+;          :when (= :site (:tag y))]
 ;          (doseq [nimi   (:content y)
 ;                  tag    (:content y)
 ;                  coords (:content y)
-;            :when (= :name   (:tag nimi))
-;            :when (= :id     (:tag tag))
-;            :when (= :coords (:tag coords))]
-;            (conj! a-list {:name (first (:content nimi)), :id (first (:content tag)), :coords (first (:content coords))}))))
-;  (for[i (range (count a-list))]
-;    (a-list i)))
+;             :when (= :name (:tag nimi))
+;             :when (= :id    (:tag tag))
+;             :when (= :coords (:tag coords))]
+;            (conj! final-list {:name (first (:content nimi)), :id (first (:content tag)), :coords (first (:content coords))})
+;            )))))
 
 
 
-(defn xml-dwarf-site-parser
-  "This function parses information about sites from the given
-  dwarven XML file path and stores it on a list"
-  [final-list file-path]
-  (let [xml-file (clojure.java.io/reader file-path)]
-    (doseq [x (xml-seq (parse xml-file :coalescing false))
-      :when (= :sites (:tag x))]
-        (doseq [y (:content x)
-          :when (= :site (:tag y))]
-          (doseq [nimi   (:content y)
-                  tag    (:content y)
-                  coords (:content y)
-             :when (= :name (:tag nimi))
-             :when (= :id    (:tag tag))
-             :when (= :coords (:tag coords))]
-            (conj! final-list {:name (first (:content nimi)), :id (first (:content tag)), :coords (first (:content coords))})
-            )))))
+(defn sites->map [element]
+  (let [z (xml-zip element)]
+      (xml1-> z)))
 
-
-
-(defn sites [e]
-  (xml1-> (xml-zip element)
-          :sites
-          text))
 
 (with-open [rdr(clojure.java.io/reader "resources/data/region2-legends.xml")]
-        (doall (->> rdr
-            parse
-            :content
-            (filter #(= :pages (:tag %)))
-            (map sites))))
+        (doall
+          (take 2
+             (->> (:content (parse rdr))
+             (filter #(= :sites (:tag %)))
+             (map sites->map)))))
 
 
 
-(def parsed-site-data (transient []))
-(xml-dwarf-site-parser parsed-site-data "resources/data/region2-legends.xml")
+;(def parsed-site-data (transient []))
+;(xml-dwarf-site-parser parsed-site-data "resources/data/region2-legends.xml")
 
 
-(for[i (range (count parsed-site-data))]
-    (parsed-site-data i))
+;(for[i (range (count parsed-site-data))]
+;    (parsed-site-data i))
 
 
-(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")]
-  (type(xml-seq (parse xml-file))))
+;(let [xml-file (clojure.java.io/reader "resources/data/region2-legends.xml")]
+;  (type(xml-seq (parse xml-file))))
